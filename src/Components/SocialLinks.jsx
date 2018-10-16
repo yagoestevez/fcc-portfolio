fb46aadd0fed2995f9e844b824e068980ca6c949
Yago Estevez <inbox@yagoestevez.com>
2018-10-16 21:43:17 +0200

    Fix formatting

--- src/Components/SocialLinks.jsx
+++ src/Components/SocialLinks.jsx
@@ -1,34 +1,38 @@
 import React from 'react';
 
 const SocialLinks = props => {
-
   return (
     <div className="social">
       <a
-        href   = "https://twitter.com/yagoestevez"
-        target = "_blank"
-        rel    = "noopener noreferrer"
-        title  = "Link to author's Twitter profile"
-      > <i className="fab fa-twitter"></i>
+        href="https://twitter.com/yagoestevez"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Link to author's Twitter profile"
+      >
+        {' '}
+        <i className="fab fa-twitter" />
       </a>
       <a
-        id     = "profile-link"
-        href   = "https://github.com/yagoestevez"
-        target = "_blank"
-        rel    = "noopener noreferrer"
-        title  = "Link to author's GitHub Profile"
-      > <i className="fab fa-github"></i>
+        id="profile-link"
+        href="https://github.com/yagoestevez"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Link to author's GitHub Profile"
+      >
+        {' '}
+        <i className="fab fa-github" />
       </a>
       <a
-        href   = "https://codepen.io/yagoestevez"
-        target = "_blank"
-        rel    = "noopener noreferrer"
-        title  = "Link to author's Codepen Profile"
-      > <i className="fab fa-codepen"></i>
+        href="https://codepen.io/yagoestevez"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Link to author's Codepen Profile"
+      >
+        {' '}
+        <i className="fab fa-codepen" />
       </a>
     </div>
   );
-
-}
+};
 
 export default SocialLinks;
